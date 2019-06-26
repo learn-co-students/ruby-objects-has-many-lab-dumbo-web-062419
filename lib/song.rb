9b70e6d74196e9fbb35a3ee42c4ef275c471feed
github username <github email address>
2019-06-26 18:21:41 -0400

    created @@all

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,20 @@
 class Song
 
-    def initialize
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
     end
-    
+
+    def artist
+        Artist.new(@name)
+    end
+
+    def all
+        @@all
+    end
+
 end
